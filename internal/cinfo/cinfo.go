cb2ea3b642e9047a46d97b61e79a8c4e037fef89
June19980 <946672819@qq.com>
2023-01-11 00:58:54 +0800

    cinfo flags

--- internal/cinfo/cinfo.go
+++ internal/cinfo/cinfo.go
@@ -5,8 +5,13 @@ import (
 	"CraneFrontEnd/internal/util"
 	"context"
 	"fmt"
+	"github.com/olekukonko/tablewriter"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 func cinfoFun() {
@@ -18,25 +23,92 @@ func cinfoFun() {
 	if err != nil {
 		panic("Cannot connect to CraneCtld: " + err.Error())
 	}
-
 	stub := protos.NewCraneCtldClient(conn)
-	req := &protos.QueryClusterInfoRequest{}
+	req := &protos.QueryClusterInfoRequest{
+		QueryDownNodes:       dead,
+		QueryRespondingNodes: responding,
+	}
+
+	partitionList := strings.Split(partitions, ",")
+	nodeList := strings.Split(nodes, ",")
+	stateReq := strings.Split(strings.ToLower(states), ",")
+	var stateList []protos.CranedState
+	if states != "" {
+		for i := 0; i < len(stateReq); i++ {
+			switch stateReq[i] {
+			case "idle":
+				stateList = append(stateList, 0)
+			case "mix":
+				stateList = append(stateList, 1)
+			case "alloc":
+				stateList = append(stateList, 2)
+			case "down":
+				stateList = append(stateList, 3)
+			default:
+				fmt.Println("Invalid states specified.")
+				os.Exit(1)
+			}
+		}
+	}
+
+	if partitions != "" {
+		req.Partitions = partitionList
+	}
+	if nodes != "" {
+		req.Nodes = nodeList
+	}
+	if states != "" {
+		req.States = stateList
+	}
 
 	reply, err := stub.QueryClusterInfo(context.Background(), req)
 	if err != nil {
 		panic("QueryClusterInfo failed: " + err.Error())
 	}
+	//fmt.Println(reply.Num)
 
-	if len(reply.PartitionCraned) == 0 {
-		fmt.Printf("No partition is available.\n")
-	} else {
-		fmt.Printf("PARTITION   AVAIL  TIMELIMIT  NODES  STATE  NODELIST\n")
-		for _, partitionCraned := range reply.PartitionCraned {
-			for _, commonCranedStateList := range partitionCraned.CommonCranedStateList {
-				if commonCranedStateList.CranedNum > 0 {
-					fmt.Printf("%9s%8s%11s%7d%7s  %v\n", partitionCraned.Name, partitionCraned.State.String(), "infinite", commonCranedStateList.CranedNum, commonCranedStateList.State.String(), commonCranedStateList.CranedListRegex)
-				}
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetBorder(false)
+	table.SetTablePadding("\t")
+	table.SetHeaderLine(false)
+	table.SetAutoWrapText(false)
+	table.SetAutoFormatHeaders(false)
+	table.SetHeaderAlignment(tablewriter.ALIGN_LEFT)
+	table.SetAlignment(tablewriter.ALIGN_LEFT)
+	table.SetCenterSeparator("")
+	table.SetColumnSeparator("")
+	table.SetRowSeparator("")
+	table.SetNoWhiteSpace(true)
+	var tableData [][]string
+	table.SetHeader([]string{"PARTITION", "AVAIL", "TIMELIMIT", "NODES", "STATE", "NODELIST"})
+	for _, partitionCraned := range reply.PartitionCraned {
+		for _, commonCranedStateList := range partitionCraned.CommonCranedStateList {
+			if commonCranedStateList.CranedNum > 0 {
+				tableData = append(tableData, []string{
+					partitionCraned.Name,
+					strings.ToLower(partitionCraned.State.String()[10:]),
+					"infinite",
+					strconv.FormatUint(uint64(commonCranedStateList.CranedNum), 10),
+					strings.ToLower(commonCranedStateList.State.String()[6:]),
+					commonCranedStateList.CranedListRegex,
+				})
 			}
 		}
+	}
+	table.AppendBulk(tableData)
+	if len(tableData) == 0 {
+		fmt.Printf("No partition is available.\n")
+	} else {
+		table.Render()
+	}
+}
+
+func IterateQuery(iterate uint64) {
+	iter, _ := time.ParseDuration(strconv.FormatUint(iterate, 10) + "s")
+	for {
+		fmt.Println(time.Now().String()[0:19])
+		cinfoFun()
+		time.Sleep(time.Duration(iter.Nanoseconds()))
+		fmt.Println()
 	}
 }
