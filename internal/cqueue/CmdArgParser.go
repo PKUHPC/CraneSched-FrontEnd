2b937bfcc133e32a8a3e4faaa257f62cbd6e903b
RileyW <wrllrwwrllrw@gmail.com>
2023-01-24 00:13:27 +0800

    Fix bugs and refactor ccancel part.

--- internal/cqueue/CmdArgParser.go
+++ internal/cqueue/CmdArgParser.go
@@ -12,14 +12,14 @@ var (
 		Use:   "cqueue [partition]",
 		Short: "display the job information for all queues in the cluster",
 		Long:  "",
-		Args:  cobra.ExactArgs(1),
+		Args:  cobra.MaximumNArgs(1),
 		PersistentPreRun: func(cmd *cobra.Command, args []string) {
 			Init()
 		},
 		Run: func(cmd *cobra.Command, args []string) {
-			// args was checked by cobra.ExactArgs(1)
-			// len(args)=1 here.
-			partition = args[0]
+			if len(args) > 0 {
+				partition = args[0]
+			}
 			Query(partition)
 		},
 	}
