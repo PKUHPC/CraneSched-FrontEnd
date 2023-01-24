98f7f5cd0a1387edd566f9e791e823c63f31ae88
June19980 <38131405+June19980@users.noreply.github.com>
2023-01-24 18:22:37 +0800

    Dev/cinfo flags (#12)
    * cinfo flags
    
    * Fix bugs and refactor cinfo part.
    
    Co-authored-by: RileyW <wrllrwwrllrw@gmail.com>

--- internal/cinfo/CmdArgParser.go
+++ internal/cinfo/CmdArgParser.go
@@ -6,21 +6,54 @@ import (
 )
 
 var (
-	rootCmd = &cobra.Command{
+	FlagFilterDownOnly       bool
+	FlagFilterRespondingOnly bool
+	FlagFilterPartitions     string
+	FlagFilterNodes          string
+	FlagFilterCranedStates   string
+	FlagSummarize            bool
+	FlagFormat               string
+	FlagIterate              uint64
+
+	RootCmd = &cobra.Command{
 		Use:   "cinfo",
 		Short: "display the status of all partitions and nodes",
 		Long:  "",
 		Run: func(cmd *cobra.Command, args []string) {
-			cinfoFun()
+			if FlagIterate != 0 {
+				loopedQuery(FlagIterate)
+			} else {
+				cinfoFunc()
+			}
 		},
 	}
 )
 
-// ParseCmdArgs executes the root command.
 func ParseCmdArgs() {
-	if err := rootCmd.Execute(); err != nil {
+	if err := RootCmd.Execute(); err != nil {
 		os.Exit(1)
 	}
 }
+
 func init() {
+	RootCmd.Flags().BoolVarP(&FlagFilterDownOnly, "dead", "d", false,
+		"show only non-responding nodes")
+	RootCmd.Flags().StringVarP(&FlagFilterPartitions, "partition", "p",
+		"", "report on specific partition")
+	RootCmd.Flags().StringVarP(&FlagFilterNodes, "nodes", "n", "",
+		"report on specific node(s)")
+	RootCmd.Flags().StringVarP(&FlagFilterCranedStates, "states", "t", "",
+		"Include craned nodes only with certain states. \n"+
+			"The state can take IDLE, MIX, ALLOC and DOWN and is case-insensitive. \n"+
+			"Example: \n"+
+			"\t -t IDLE,mIx \n"+
+			"\t -t=Alloc \n")
+	RootCmd.Flags().BoolVarP(&FlagFilterRespondingOnly, "responding", "r", false,
+		"report only responding nodes")
+	RootCmd.Flags().Uint64VarP(&FlagIterate, "iterate", "i", 0,
+		"specify an interval in seconds")
+	RootCmd.Flags().BoolVarP(&FlagSummarize, "summarize", "s", false,
+		"report state summary only")
+	RootCmd.Flags().StringVarP(&FlagFormat, "format", "o", "",
+		"format specification")
 }
--- internal/cinfo/cinfo.go
+++ internal/cinfo/cinfo.go
@@ -5,12 +5,16 @@ import (
 	"CraneFrontEnd/internal/util"
 	"context"
 	"fmt"
+	"github.com/olekukonko/tablewriter"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
-func cinfoFun() {
-
+func cinfoFunc() {
 	config := util.ParseConfig()
 
 	serverAddr := fmt.Sprintf("%s:%s", config.ControlMachine, config.CraneCtldListenPort)
@@ -18,25 +22,90 @@ func cinfoFun() {
 	if err != nil {
 		panic("Cannot connect to CraneCtld: " + err.Error())
 	}
-
 	stub := protos.NewCraneCtldClient(conn)
-	req := &protos.QueryClusterInfoRequest{}
+	req := &protos.QueryClusterInfoRequest{
+		FilterOnlyDownNodes:       FlagFilterDownOnly,
+		FilterOnlyRespondingNodes: FlagFilterRespondingOnly,
+	}
+
+	var stateList []protos.CranedState
+	if FlagFilterCranedStates != "" {
+		filterCranedStateList := strings.Split(strings.ToLower(FlagFilterCranedStates), ",")
+		for i := 0; i < len(filterCranedStateList); i++ {
+			switch filterCranedStateList[i] {
+			case "idle":
+				stateList = append(stateList, protos.CranedState_CRANE_IDLE)
+			case "mix":
+				stateList = append(stateList, protos.CranedState_CRANE_MIX)
+			case "alloc":
+				stateList = append(stateList, protos.CranedState_CRANE_ALLOC)
+			case "down":
+				stateList = append(stateList, protos.CranedState_CRANE_DOWN)
+			default:
+				fmt.Fprintf(os.Stderr, "Invalid state given: %s\n", filterCranedStateList[i])
+				os.Exit(1)
+			}
+		}
+		req.FilterCranedStates = stateList
+	}
+
+	if FlagFilterPartitions != "" {
+		filterPartitionList := strings.Split(FlagFilterPartitions, ",")
+		req.FilterPartitions = filterPartitionList
+	}
+
+	if FlagFilterNodes != "" {
+		filterNodeList := strings.Split(FlagFilterNodes, ",")
+		req.FilterNodes = filterNodeList
+	}
 
 	reply, err := stub.QueryClusterInfo(context.Background(), req)
 	if err != nil {
 		panic("QueryClusterInfo failed: " + err.Error())
 	}
 
-	if len(reply.PartitionCraned) == 0 {
-		fmt.Printf("No partition is available.\n")
-	} else {
-		fmt.Printf("PARTITION   AVAIL  TIMELIMIT  NODES  STATE  NODELIST\n")
-		for _, partitionCraned := range reply.PartitionCraned {
-			for _, commonCranedStateList := range partitionCraned.CommonCranedStateList {
-				if commonCranedStateList.CranedNum > 0 {
-					fmt.Printf("%9s%8s%11s%7d%7s  %v\n", partitionCraned.Name, partitionCraned.State.String(), "infinite", commonCranedStateList.CranedNum, commonCranedStateList.State.String(), commonCranedStateList.CranedListRegex)
-				}
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetBorder(false)
+	table.SetTablePadding("\t")
+	table.SetHeaderLine(false)
+	table.SetAutoWrapText(false)
+	table.SetAutoFormatHeaders(false)
+	table.SetHeaderAlignment(tablewriter.ALIGN_LEFT)
+	table.SetAlignment(tablewriter.ALIGN_LEFT)
+	table.SetCenterSeparator("")
+	table.SetColumnSeparator("")
+	table.SetRowSeparator("")
+	table.SetNoWhiteSpace(true)
+	var tableData [][]string
+	table.SetHeader([]string{"PARTITION", "AVAIL", "TIMELIMIT", "NODES", "STATE", "NODELIST"})
+	for _, partitionCraned := range reply.Partitions {
+		for _, commonCranedStateList := range partitionCraned.CranedLists {
+			if commonCranedStateList.Count > 0 {
+				tableData = append(tableData, []string{
+					partitionCraned.Name,
+					strings.ToLower(partitionCraned.State.String()[10:]),
+					"infinite",
+					strconv.FormatUint(uint64(commonCranedStateList.Count), 10),
+					strings.ToLower(commonCranedStateList.State.String()[6:]),
+					commonCranedStateList.CranedListRegex,
+				})
 			}
 		}
+	}
+	table.AppendBulk(tableData)
+	if len(tableData) == 0 {
+		fmt.Printf("No partition is available.\n")
+	} else {
+		table.Render()
+	}
+}
+
+func loopedQuery(iterate uint64) {
+	interval, _ := time.ParseDuration(strconv.FormatUint(iterate, 10) + "s")
+	for {
+		fmt.Println(time.Now().String()[0:19])
+		cinfoFunc()
+		time.Sleep(time.Duration(interval.Nanoseconds()))
+		fmt.Println()
 	}
 }
