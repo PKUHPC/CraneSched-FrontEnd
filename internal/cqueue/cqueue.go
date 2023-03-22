11d0d4e00085b6e330169f2257034b67254c8111
June19980 <946672819@qq.com>
2023-03-22 15:33:54 +0800

    cqueue flags

--- internal/cqueue/cqueue.go
+++ internal/cqueue/cqueue.go
@@ -3,23 +3,75 @@ package cqueue
 import (
 	"CraneFrontEnd/generated/protos"
 	"context"
+	"fmt"
 	"github.com/olekukonko/tablewriter"
 	"os"
 	"strconv"
+	"strings"
+	"time"
 )
 
 var (
 	stub protos.CraneCtldClient
 )
 
-func Query(partition string) {
-	request := protos.QueryTasksInfoRequest{
-		Partition: partition,
-		TaskId:    -1,
+func Query() {
+	req := protos.QueryTasksInfoRequest{}
+
+	var stateList []protos.TaskStatus
+	if FlagFilterStates != "" {
+		filterStateList := strings.Split(strings.ToLower(FlagFilterStates), ",")
+		for i := 0; i < len(filterStateList); i++ {
+			switch filterStateList[i] {
+			case "r", "running":
+				stateList = append(stateList, protos.TaskStatus_Running)
+			case "p", "pending":
+				stateList = append(stateList, protos.TaskStatus_Pending)
+			case "cancelled":
+				stateList = append(stateList, protos.TaskStatus_Cancelled)
+			case "completing":
+				stateList = append(stateList, protos.TaskStatus_Completing)
+			default:
+				fmt.Fprintf(os.Stderr, "Invalid state given: %s\n", filterStateList[i])
+				os.Exit(1)
+			}
+		}
+		req.FilterTaskStates = stateList
 	}
-	reply, err := stub.QueryTasksInfo(context.Background(), &request)
+
+	if FlagFilterJobNames != "" {
+		filterJobNameList := strings.Split(FlagFilterJobNames, ",")
+		req.FilterTaskNames = filterJobNameList
+	}
+	if FlagFilterUsers != "" {
+		filterUserList := strings.Split(FlagFilterUsers, ",")
+		req.FilterUsers = filterUserList
+	}
+	if FlagFilterAccounts != "" {
+		filterAccountList := strings.Split(FlagFilterAccounts, ",")
+		req.FilterAccounts = filterAccountList
+	}
+	if FlagFilterPartitions != "" {
+		filterPartitionList := strings.Split(FlagFilterPartitions, ",")
+		req.FilterPartitions = filterPartitionList
+	}
+	if FlagFilterJobIDs != "" {
+		filterJobIdList := strings.Split(FlagFilterPartitions, ",")
+		var filterJobIdListInt []uint32
+		for i := 0; i < len(filterJobIdList); i++ {
+			id, err := strconv.ParseUint(filterJobIdList[i], 10, 32)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Invalid task id given: %s\n", filterJobIdList[i])
+				os.Exit(1)
+			}
+			filterJobIdListInt[i] = uint32(id)
+		}
+		req.FilterTaskIds = filterJobIdListInt
+	}
+
+	reply, err := stub.QueryTasksInfo(context.Background(), &req)
 	if err != nil {
-		panic("QueryJobsInPartition failed: " + err.Error())
+		panic("QueryTasksInfo failed: " + err.Error())
 	}
 
 	table := tablewriter.NewWriter(os.Stdout)
@@ -34,18 +86,45 @@ func Query(partition string) {
 	table.SetColumnSeparator("")
 	table.SetRowSeparator("")
 	table.SetNoWhiteSpace(true)
-
-	table.SetHeader([]string{"TaskId", "Type", "Status", "NodeIndex"})
+	if !FlagNoHeader {
+		if FlagStartTime {
+			table.SetHeader([]string{"TaskId", "Name", "Type", "Status", "StartTime", "NodeIndex"})
+		} else {
+			table.SetHeader([]string{"TaskId", "Name", "Type", "Status", "NodeIndex"})
+		}
+	}
 
 	tableData := make([][]string, len(reply.TaskInfoList))
 	for i := 0; i < len(reply.TaskInfoList); i++ {
-		tableData = append(tableData, []string{
-			strconv.FormatUint(uint64(reply.TaskInfoList[i].TaskId), 10),
-			reply.TaskInfoList[i].Type.String(),
-			reply.TaskInfoList[i].Status.String(),
-			reply.TaskInfoList[i].CranedList})
+		if FlagStartTime {
+			tableData = append(tableData, []string{
+				strconv.FormatUint(uint64(reply.TaskInfoList[i].TaskId), 10),
+				reply.TaskInfoList[i].Name,
+				//reply.TaskInfoList[i].UserName,
+				reply.TaskInfoList[i].Type.String(),
+				reply.TaskInfoList[i].Status.String(),
+				reply.TaskInfoList[i].StartTime.String(),
+				reply.TaskInfoList[i].CranedList})
+		} else {
+			tableData = append(tableData, []string{
+				strconv.FormatUint(uint64(reply.TaskInfoList[i].TaskId), 10),
+				reply.TaskInfoList[i].Name,
+				reply.TaskInfoList[i].Type.String(),
+				reply.TaskInfoList[i].Status.String(),
+				reply.TaskInfoList[i].CranedList})
+		}
 	}
 
 	table.AppendBulk(tableData)
 	table.Render()
+}
+
+func loopedQuery(iterate uint64) {
+	interval, _ := time.ParseDuration(strconv.FormatUint(iterate, 10) + "s")
+	for {
+		fmt.Println(time.Now().String()[0:19])
+		Query()
+		time.Sleep(time.Duration(interval.Nanoseconds()))
+		fmt.Println()
+	}
 }
