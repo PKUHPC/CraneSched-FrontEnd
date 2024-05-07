5240739f1219364c700b2bb32e93100380b3a7db
lixinyan00 <lixinyan00@126.com>
2024-05-07 21:38:07 +0800

    add ccontrol show config and update priority
    Signed-off-by: lixinyan00 <lixinyan00@126.com>

--- internal/ccontrol/CmdArgParser.go
+++ internal/ccontrol/CmdArgParser.go
@@ -14,115 +14,139 @@
  * See the Mulan PSL v2 for more details.
  */
 
-package ccontrol
+ package ccontrol
 
-import (
-	"CraneFrontEnd/internal/util"
-	"github.com/spf13/cobra"
-	"os"
-	"strconv"
-)
-
-var (
-	FlagNodeName      string
-	FlagPartitionName string
-	FlagTaskId        uint32
-	FlagQueryAll      bool
-	FlagTimeLimit     string
-
-	FlagConfigFilePath string
-
-	rootCmd = &cobra.Command{
-		Use:   "ccontrol",
-		Short: "display the state of partitions and nodes",
-		Long:  "",
-		PersistentPreRun: func(cmd *cobra.Command, args []string) {
-			config := util.ParseConfig(FlagConfigFilePath)
-			stub = util.GetStubToCtldByConfig(config)
-		},
-	}
-	showCmd = &cobra.Command{
-		Use:   "show",
-		Short: "display state of identified entity, default is all records",
-		Long:  "",
-	}
-	showNodeCmd = &cobra.Command{
-		Use:   "node",
-		Short: "display state of the specified node, default is all records",
-		Long:  "",
-		Args:  cobra.MaximumNArgs(1),
-		Run: func(cmd *cobra.Command, args []string) {
-			if len(args) == 0 {
-				FlagNodeName = ""
-				FlagQueryAll = true
-			} else {
-				FlagNodeName = args[0]
-				FlagQueryAll = false
-			}
-			ShowNodes(FlagNodeName, FlagQueryAll)
-		},
-	}
-	showPartitionCmd = &cobra.Command{
-		Use:   "partition",
-		Short: "display state of the specified partition, default is all records",
-		Long:  "",
-		Args:  cobra.MaximumNArgs(1),
-		Run: func(cmd *cobra.Command, args []string) {
-			if len(args) == 0 {
-				FlagPartitionName = ""
-				FlagQueryAll = true
-			} else {
-				FlagPartitionName = args[0]
-				FlagQueryAll = false
-			}
-			ShowPartitions(FlagPartitionName, FlagQueryAll)
-		},
-	}
-	showTaskCmd = &cobra.Command{
-		Use:   "job",
-		Short: "display the state of a specified job or all jobs",
-		Long:  "",
-		Args:  cobra.MaximumNArgs(1),
-		Run: func(cmd *cobra.Command, args []string) {
-			if len(args) == 0 {
-				FlagQueryAll = true
-			} else {
-				id, _ := strconv.Atoi(args[0])
-				FlagTaskId = uint32(id)
-				FlagQueryAll = false
-			}
-			ShowTasks(FlagTaskId, FlagQueryAll)
-		},
-	}
-	updateCmd = &cobra.Command{
-		Use:   "update",
-		Short: "Modify job information",
-		Long:  "",
-		Run: func(cmd *cobra.Command, args []string) {
-			ChangeTaskTimeLimit(FlagTaskId, FlagTimeLimit)
-		},
-	}
-)
-
-// ParseCmdArgs executes the root command.
-func ParseCmdArgs() {
-	if err := rootCmd.Execute(); err != nil {
-		os.Exit(1)
-	}
-}
-
-func init() {
-	rootCmd.AddCommand(showCmd)
-	rootCmd.PersistentFlags().StringVarP(&FlagConfigFilePath, "config", "C", util.DefaultConfigPath,
-		"Path to configuration file")
-	showCmd.AddCommand(showNodeCmd)
-	showCmd.AddCommand(showPartitionCmd)
-	showCmd.AddCommand(showTaskCmd)
-	rootCmd.AddCommand(updateCmd)
-	updateCmd.Flags().Uint32VarP(&FlagTaskId, "job", "J", 0, "Job id")
-	updateCmd.Flags().StringVarP(&FlagTimeLimit, "time-limit", "T", "", "time limit")
-	err := updateCmd.MarkFlagRequired("job")
-	if err != nil {
-		return
-	}
-}
+ import (
+	 "CraneFrontEnd/internal/util"
+	 "github.com/spf13/cobra"
+	 "os"
+	 "strconv"
+ )
+ 
+ var (
+	 FlagNodeName      string
+	 FlagPartitionName string
+	 FlagTaskId        uint32
+	 FlagQueryAll      bool
+	 FlagTimeLimit     string
+	 FlagPriority	  uint32
+	 FlagConfigFilePath string
+ 
+	 ConfigPath string
+ 
+	 rootCmd = &cobra.Command{
+		 Use:   "ccontrol",
+		 Short: "display the state of partitions and nodes",
+		 Long:  "",
+		 PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			 config := util.ParseConfig(FlagConfigFilePath)
+			 stub = util.GetStubToCtldByConfig(config)
+		 },
+	 }
+	 showCmd = &cobra.Command{
+		 Use:   "show",
+		 Short: "display state of identified entity, default is all records",
+		 Long:  "",
+	 }
+	 showNodeCmd = &cobra.Command{
+		 Use:   "node",
+		 Short: "display state of the specified node, default is all records",
+		 Long:  "",
+		 Args:  cobra.MaximumNArgs(1),
+		 Run: func(cmd *cobra.Command, args []string) {
+			 if len(args) == 0 {
+				 FlagNodeName = ""
+				 FlagQueryAll = true
+			 } else {
+				 FlagNodeName = args[0]
+				 FlagQueryAll = false
+			 }
+			 ShowNodes(FlagNodeName, FlagQueryAll)
+		 },
+	 }
+	 showPartitionCmd = &cobra.Command{
+		 Use:   "partition",
+		 Short: "display state of the specified partition, default is all records",
+		 Long:  "",
+		 Args:  cobra.MaximumNArgs(1),
+		 Run: func(cmd *cobra.Command, args []string) {
+			 if len(args) == 0 {
+				 FlagPartitionName = ""
+				 FlagQueryAll = true
+			 } else {
+				 FlagPartitionName = args[0]
+				 FlagQueryAll = false
+			 }
+			 ShowPartitions(FlagPartitionName, FlagQueryAll)
+		 },
+	 }
+	 showTaskCmd = &cobra.Command{
+		 Use:   "job",
+		 Short: "display the state of a specified job or all jobs",
+		 Long:  "",
+		 Args:  cobra.MaximumNArgs(1),
+		 Run: func(cmd *cobra.Command, args []string) {
+			 if len(args) == 0 {
+				 FlagQueryAll = true
+			 } else {
+				 id, _ := strconv.Atoi(args[0])
+				 FlagTaskId = uint32(id)
+				 FlagQueryAll = false
+			 }
+			 ShowTasks(FlagTaskId, FlagQueryAll)
+		 },
+	 }
+	 showConfigCmd = &cobra.Command{
+		 Use:   "config",
+		 Short: "display the configuration file(default is /etc/crane/config.yaml)",
+		 Long:  "",
+		 Args:  cobra.MaximumNArgs(1),
+		 Run: func(cmd *cobra.Command, args []string) {
+			 if len(args) == 0 {
+				 ConfigPath = util.DefaultConfigPath
+			 } else {
+				 ConfigPath = args[0]
+			 }
+			 ShowConfig(ConfigPath)
+		 },
+	 }
+	 updateCmd = &cobra.Command{
+		 Use:   "update",
+		 Short: "Modify job information",
+		 Long:  "",
+		 Run: func(cmd *cobra.Command, args []string) {
+			 if len(FlagTimeLimit) != 0 {
+				 ChangeTaskTimeLimit(FlagTaskId, FlagTimeLimit)
+			 }
+			 if FlagPriority != 0 {
+				 ChangeTaskPriority(FlagTaskId, FlagPriority)
+			 }
+		 },
+	 }
+ )
+ 
+ // ParseCmdArgs executes the root command.
+ func ParseCmdArgs() {
+	 if err := rootCmd.Execute(); err != nil {
+		 os.Exit(1)
+	 }
+ }
+ 
+ func init() {
+	 rootCmd.AddCommand(showCmd)
+	 rootCmd.PersistentFlags().StringVarP(&FlagConfigFilePath, "config", "C", util.DefaultConfigPath,
+		 "Path to configuration file")
+	 showCmd.AddCommand(showNodeCmd)
+	 showCmd.AddCommand(showPartitionCmd)
+	 showCmd.AddCommand(showTaskCmd)
+	 showCmd.AddCommand(showConfigCmd)
+	 rootCmd.AddCommand(updateCmd)
+	 updateCmd.Flags().Uint32VarP(&FlagTaskId, "job", "J", 0, "Job id")
+	 updateCmd.Flags().StringVarP(&FlagTimeLimit, "time-limit", "T", "", "time limit")
+	 updateCmd.Flags().Uint32VarP(&FlagPriority, "priority", "P", 0, "Job priority")
+	 err := updateCmd.MarkFlagRequired("job")
+	 if err != nil {
+		 return
+	 }
+ }
+ 
--- internal/ccontrol/ccontrol.go
+++ internal/ccontrol/ccontrol.go
@@ -14,225 +14,290 @@
  * See the Mulan PSL v2 for more details.
  */
 
-package ccontrol
+ package ccontrol
 
-import (
-	"CraneFrontEnd/generated/protos"
-	"CraneFrontEnd/internal/util"
-	"context"
-	"fmt"
-	log "github.com/sirupsen/logrus"
-	"math"
-	"os"
-	"regexp"
-	"strconv"
-	"strings"
-	"time"
-)
-
-var (
-	stub protos.CraneCtldClient
-)
-
-func ShowNodes(nodeName string, queryAll bool) {
-	var req *protos.QueryCranedInfoRequest
-
-	req = &protos.QueryCranedInfoRequest{CranedName: nodeName}
-	reply, err := stub.QueryCranedInfo(context.Background(), req)
-	if err != nil {
-		util.GrpcErrorPrintf(err, "Failed to show nodes")
-	}
-
-	var B2MBRatio uint64 = 1024 * 1024
-
-	if queryAll {
-		if len(reply.CranedInfoList) == 0 {
-			fmt.Printf("No node is avalable.\n")
-		} else {
-			for _, nodeInfo := range reply.CranedInfoList {
-				fmt.Printf("NodeName=%v State=%v CPU=%.2f AllocCPU=%.2f FreeCPU=%.2f\n"+
-					"\tRealMemory=%dM AllocMem=%dM FreeMem=%dM\n"+
-					"\tPatition=%s RunningJob=%d\n\n",
-					nodeInfo.Hostname, nodeInfo.State.String()[6:], nodeInfo.Cpu,
-					math.Abs(nodeInfo.AllocCpu),
-					math.Abs(nodeInfo.FreeCpu),
-					nodeInfo.RealMem/B2MBRatio, nodeInfo.AllocMem/B2MBRatio, nodeInfo.FreeMem/B2MBRatio,
-					strings.Join(nodeInfo.PartitionNames, ","), nodeInfo.RunningTaskNum)
-			}
-		}
-	} else {
-		if len(reply.CranedInfoList) == 0 {
-			fmt.Printf("Node %s not found.\n", nodeName)
-		} else {
-			for _, nodeInfo := range reply.CranedInfoList {
-				fmt.Printf("NodeName=%v State=%v CPU=%.2f AllocCPU=%.2f FreeCPU=%.2f\n"+
-					"\tRealMemory=%dM AllocMem=%dM FreeMem=%dM\n"+
-					"\tPatition=%s RunningJob=%d\n\n",
-					nodeInfo.Hostname, nodeInfo.State.String()[6:], nodeInfo.Cpu, nodeInfo.AllocCpu, nodeInfo.FreeCpu,
-					nodeInfo.RealMem/B2MBRatio, nodeInfo.AllocMem/B2MBRatio, nodeInfo.FreeMem/B2MBRatio,
-					strings.Join(nodeInfo.PartitionNames, ","), nodeInfo.RunningTaskNum)
-			}
-		}
-	}
-}
-
-func ShowPartitions(partitionName string, queryAll bool) {
-	var req *protos.QueryPartitionInfoRequest
-
-	req = &protos.QueryPartitionInfoRequest{PartitionName: partitionName}
-	reply, err := stub.QueryPartitionInfo(context.Background(), req)
-	if err != nil {
-		util.GrpcErrorPrintf(err, "Failed to show partition")
-	}
-
-	var B2MBRatio uint64 = 1024 * 1024
-
-	if queryAll {
-		if len(reply.PartitionInfo) == 0 {
-			fmt.Printf("No node is avalable.\n")
-		} else {
-			for _, partitionInfo := range reply.PartitionInfo {
-				fmt.Printf("PartitionName=%v State=%v\n"+
-					"\tTotalNodes=%d AliveNodes=%d\n"+
-					"\tTotalCPU=%.2f AvailCPU=%.2f AllocCPU=%.2f\n"+
-					"\tTotalMem=%dM AvailMem=%dM AllocMem=%dM\n\tHostList=%v\n\n",
-					partitionInfo.Name, partitionInfo.State.String()[10:],
-					partitionInfo.TotalNodes, partitionInfo.AliveNodes,
-					partitionInfo.TotalCpu, partitionInfo.AvailCpu, partitionInfo.AllocCpu,
-					partitionInfo.TotalMem/B2MBRatio, partitionInfo.AvailMem/B2MBRatio, partitionInfo.AllocMem/B2MBRatio, partitionInfo.Hostlist)
-			}
-		}
-	} else {
-		if len(reply.PartitionInfo) == 0 {
-			fmt.Printf("Partition %s not found.\n", partitionName)
-		} else {
-			for _, partitionInfo := range reply.PartitionInfo {
-				fmt.Printf("PartitionName=%v State=%v\n"+
-					"\tTotalNodes=%d AliveNodes=%d\n"+
-					"\tTotalCPU=%.2f AvailCPU=%.2f AllocCPU=%.2f\n"+
-					"\tTotalMem=%dM AvailMem=%dM AllocMem=%dM\n\tHostList=%v\n\n",
-					partitionInfo.Name, partitionInfo.State.String()[10:],
-					partitionInfo.TotalNodes, partitionInfo.AliveNodes,
-					partitionInfo.TotalCpu, partitionInfo.AvailCpu, partitionInfo.AllocCpu,
-					partitionInfo.TotalMem/B2MBRatio, partitionInfo.AvailMem/B2MBRatio, partitionInfo.AllocMem/B2MBRatio, partitionInfo.Hostlist)
-			}
-		}
-	}
-}
-
-func ShowTasks(taskId uint32, queryAll bool) {
-	var req *protos.QueryTasksInfoRequest
-	var taskIdList []uint32
-	taskIdList = append(taskIdList, taskId)
-	if queryAll {
-		req = &protos.QueryTasksInfoRequest{}
-	} else {
-		req = &protos.QueryTasksInfoRequest{FilterTaskIds: taskIdList}
-	}
-
-	reply, err := stub.QueryTasksInfo(context.Background(), req)
-	if err != nil {
-		util.GrpcErrorPrintf(err, "Failed to show the task")
-	}
-
-	if !reply.GetOk() {
-		log.Errorf("Failed to retrive the information of job %d", taskId)
-		os.Exit(1)
-	}
-
-	if len(reply.TaskInfoList) == 0 {
-		if queryAll {
-			fmt.Printf("No job is running.\n")
-		} else {
-			fmt.Printf("Job %d is not running.\n", taskId)
-		}
-
-	} else {
-		for _, taskInfo := range reply.TaskInfoList {
-			timeSubmit := taskInfo.SubmitTime.AsTime()
-			timeSubmitStr := "unknown"
-			if !timeSubmit.Before(time.Date(1980, 1, 1, 0, 0, 0, 0, time.UTC)) {
-				timeSubmitStr = timeSubmit.Local().String()
-			}
-			timeStart := taskInfo.StartTime.AsTime()
-			timeStartStr := "unknown"
-			runTime := "unknown"
-			if !timeStart.Before(time.Date(1980, 1, 1, 0, 0, 0, 0, time.UTC)) {
-				timeStartStr = timeStart.Local().String()
-				runTime = time.Now().Sub(timeStart).String()
-			}
-			timeEnd := taskInfo.EndTime.AsTime()
-			timeEndStr := "unknown"
-			if timeEnd.After(timeStart) {
-				timeEndStr = timeEnd.Local().String()
-				runTime = timeEnd.Sub(timeStart).String()
-			}
-			if taskInfo.Status.String() == "Running" {
-				timeEndStr = timeStart.Add(taskInfo.TimeLimit.AsDuration()).String()
-			}
-			var timeLimitStr string
-			if taskInfo.TimeLimit.Seconds >= util.InvalidDuration().Seconds {
-				timeLimitStr = "unlimited"
-				timeEndStr = "unknown"
-			} else {
-				timeLimitStr = util.SecondTimeFormat(taskInfo.TimeLimit.Seconds)
-			}
-
-			fmt.Printf("JobId=%v JobName=%v\n\tUserId=%d GroupId=%d Account=%v\n\tJobState=%v RunTime=%v "+
-				"TimeLimit=%s SubmitTime=%v\n\tStartTime=%v EndTime=%v Partition=%v NodeList=%v "+
-				"NumNodes=%d\n\tCmdLine=%v Workdir=%v\n",
-				taskInfo.TaskId, taskInfo.Name, taskInfo.Uid, taskInfo.Gid,
-				taskInfo.Account, taskInfo.Status.String(), runTime, timeLimitStr,
-				timeSubmitStr, timeStartStr, timeEndStr, taskInfo.Partition,
-				taskInfo.CranedList, taskInfo.NodeNum, taskInfo.CmdLine, taskInfo.Cwd)
-		}
-	}
-}
-
-func ChangeTaskTimeLimit(taskId uint32, timeLimit string) {
-	re := regexp.MustCompile(`((.*)-)?(.*):(.*):(.*)`)
-	result := re.FindAllStringSubmatch(timeLimit, -1)
-
-	if result == nil || len(result) != 1 {
-		log.Fatalf("Time format error")
-	}
-	var dd uint64
-	if result[0][2] != "" {
-		dd, _ = strconv.ParseUint(result[0][2], 10, 32)
-	}
-	hh, err := strconv.ParseUint(result[0][3], 10, 32)
-	if err != nil {
-		log.Fatalf("The hour time format error")
-	}
-	mm, err := strconv.ParseUint(result[0][4], 10, 32)
-	if err != nil {
-		log.Fatalf("The minute time format error")
-	}
-	ss, err := strconv.ParseUint(result[0][5], 10, 32)
-	if err != nil {
-		log.Fatalf("The second time format error")
-	}
-
-	seconds := int64(60*60*24*dd + 60*60*hh + 60*mm + ss)
-
-	var req *protos.ModifyTaskRequest
-
-	req = &protos.ModifyTaskRequest{
-		Uid:       uint32(os.Getuid()),
-		TaskId:    taskId,
-		Attribute: protos.ModifyTaskRequest_TimeLimit,
-		Value: &protos.ModifyTaskRequest_TimeLimitSeconds{
-			TimeLimitSeconds: seconds,
-		},
-	}
-	reply, err := stub.ModifyTask(context.Background(), req)
-	if err != nil {
-		util.GrpcErrorPrintf(err, "Failed to change task time limit")
-	}
-
-	if reply.Ok {
-		fmt.Println("Change time limit success")
-	} else {
-		fmt.Printf("Chang time limit failed: %s\n", reply.GetReason())
-	}
-}
+ import (
+	 "CraneFrontEnd/generated/protos"
+	 "CraneFrontEnd/internal/util"
+	 "context"
+	 "fmt"
+	 log "github.com/sirupsen/logrus"
+	 "math"
+	 "os"
+	 "regexp"
+	 "strconv"
+	 "strings"
+	 "time"
+	 "io/ioutil"
+	 "gopkg.in/yaml.v2"
+	 "reflect"
+ )
+ 
+ var (
+	 stub protos.CraneCtldClient
+ )
+ 
+ 
+ func ShowNodes(nodeName string, queryAll bool) {
+	 var req *protos.QueryCranedInfoRequest
+ 
+	 req = &protos.QueryCranedInfoRequest{CranedName: nodeName}
+	 reply, err := stub.QueryCranedInfo(context.Background(), req)
+	 if err != nil {
+		 util.GrpcErrorPrintf(err, "Failed to show nodes")
+	 }
+ 
+	 var B2MBRatio uint64 = 1024 * 1024
+ 
+	 if queryAll {
+		 if len(reply.CranedInfoList) == 0 {
+			 fmt.Printf("No node is avalable.\n")
+		 } else {
+			 for _, nodeInfo := range reply.CranedInfoList {
+				 fmt.Printf("NodeName=%v State=%v CPU=%.2f AllocCPU=%.2f FreeCPU=%.2f\n"+
+					 "\tRealMemory=%dM AllocMem=%dM FreeMem=%dM\n"+
+					 "\tPatition=%s RunningJob=%d\n\n",
+					 nodeInfo.Hostname, nodeInfo.State.String()[6:], nodeInfo.Cpu,
+					 math.Abs(nodeInfo.AllocCpu),
+					 math.Abs(nodeInfo.FreeCpu),
+					 nodeInfo.RealMem/B2MBRatio, nodeInfo.AllocMem/B2MBRatio, nodeInfo.FreeMem/B2MBRatio,
+					 strings.Join(nodeInfo.PartitionNames, ","), nodeInfo.RunningTaskNum)
+			 }
+		 }
+	 } else {
+		 if len(reply.CranedInfoList) == 0 {
+			 fmt.Printf("Node %s not found.\n", nodeName)
+		 } else {
+			 for _, nodeInfo := range reply.CranedInfoList {
+				 fmt.Printf("NodeName=%v State=%v CPU=%.2f AllocCPU=%.2f FreeCPU=%.2f\n"+
+					 "\tRealMemory=%dM AllocMem=%dM FreeMem=%dM\n"+
+					 "\tPatition=%s RunningJob=%d\n\n",
+					 nodeInfo.Hostname, nodeInfo.State.String()[6:], nodeInfo.Cpu, nodeInfo.AllocCpu, nodeInfo.FreeCpu,
+					 nodeInfo.RealMem/B2MBRatio, nodeInfo.AllocMem/B2MBRatio, nodeInfo.FreeMem/B2MBRatio,
+					 strings.Join(nodeInfo.PartitionNames, ","), nodeInfo.RunningTaskNum)
+			 }
+		 }
+	 }
+ }
+ 
+ func ShowPartitions(partitionName string, queryAll bool) {
+	 var req *protos.QueryPartitionInfoRequest
+ 
+	 req = &protos.QueryPartitionInfoRequest{PartitionName: partitionName}
+	 reply, err := stub.QueryPartitionInfo(context.Background(), req)
+	 if err != nil {
+		 util.GrpcErrorPrintf(err, "Failed to show partition")
+	 }
+ 
+	 var B2MBRatio uint64 = 1024 * 1024
+ 
+	 if queryAll {
+		 if len(reply.PartitionInfo) == 0 {
+			 fmt.Printf("No node is avalable.\n")
+		 } else {
+			 for _, partitionInfo := range reply.PartitionInfo {
+				 fmt.Printf("PartitionName=%v State=%v\n"+
+					 "\tTotalNodes=%d AliveNodes=%d\n"+
+					 "\tTotalCPU=%.2f AvailCPU=%.2f AllocCPU=%.2f\n"+
+					 "\tTotalMem=%dM AvailMem=%dM AllocMem=%dM\n\tHostList=%v\n\n",
+					 partitionInfo.Name, partitionInfo.State.String()[10:],
+					 partitionInfo.TotalNodes, partitionInfo.AliveNodes,
+					 partitionInfo.TotalCpu, partitionInfo.AvailCpu, partitionInfo.AllocCpu,
+					 partitionInfo.TotalMem/B2MBRatio, partitionInfo.AvailMem/B2MBRatio, partitionInfo.AllocMem/B2MBRatio, partitionInfo.Hostlist)
+			 }
+		 }
+	 } else {
+		 if len(reply.PartitionInfo) == 0 {
+			 fmt.Printf("Partition %s not found.\n", partitionName)
+		 } else {
+			 for _, partitionInfo := range reply.PartitionInfo {
+				 fmt.Printf("PartitionName=%v State=%v\n"+
+					 "\tTotalNodes=%d AliveNodes=%d\n"+
+					 "\tTotalCPU=%.2f AvailCPU=%.2f AllocCPU=%.2f\n"+
+					 "\tTotalMem=%dM AvailMem=%dM AllocMem=%dM\n\tHostList=%v\n\n",
+					 partitionInfo.Name, partitionInfo.State.String()[10:],
+					 partitionInfo.TotalNodes, partitionInfo.AliveNodes,
+					 partitionInfo.TotalCpu, partitionInfo.AvailCpu, partitionInfo.AllocCpu,
+					 partitionInfo.TotalMem/B2MBRatio, partitionInfo.AvailMem/B2MBRatio, partitionInfo.AllocMem/B2MBRatio, partitionInfo.Hostlist)
+			 }
+		 }
+	 }
+ }
+ 
+ func ShowTasks(taskId uint32, queryAll bool) {
+	 var req *protos.QueryTasksInfoRequest
+	 var taskIdList []uint32
+	 taskIdList = append(taskIdList, taskId)
+	 if queryAll {
+		 req = &protos.QueryTasksInfoRequest{}
+	 } else {
+		 req = &protos.QueryTasksInfoRequest{FilterTaskIds: taskIdList}
+	 }
+ 
+	 reply, err := stub.QueryTasksInfo(context.Background(), req)
+	 if err != nil {
+		 util.GrpcErrorPrintf(err, "Failed to show the task")
+	 }
+ 
+	 if !reply.GetOk() {
+		 log.Errorf("Failed to retrive the information of job %d", taskId)
+		 os.Exit(1)
+	 }
+ 
+	 if len(reply.TaskInfoList) == 0 {
+		 if queryAll {
+			 fmt.Printf("No job is running.\n")
+		 } else {
+			 fmt.Printf("Job %d is not running.\n", taskId)
+		 }
+ 
+	 } else {
+		 for _, taskInfo := range reply.TaskInfoList {
+			 timeSubmit := taskInfo.SubmitTime.AsTime()
+			 timeSubmitStr := "unknown"
+			 if !timeSubmit.Before(time.Date(1980, 1, 1, 0, 0, 0, 0, time.UTC)) {
+				 timeSubmitStr = timeSubmit.Local().String()
+			 }
+			 timeStart := taskInfo.StartTime.AsTime()
+			 timeStartStr := "unknown"
+			 runTime := "unknown"
+			 if !timeStart.Before(time.Date(1980, 1, 1, 0, 0, 0, 0, time.UTC)) {
+				 timeStartStr = timeStart.Local().String()
+				 runTime = time.Now().Sub(timeStart).String()
+			 }
+			 timeEnd := taskInfo.EndTime.AsTime()
+			 timeEndStr := "unknown"
+			 if timeEnd.After(timeStart) {
+				 timeEndStr = timeEnd.Local().String()
+				 runTime = timeEnd.Sub(timeStart).String()
+			 }
+			 if taskInfo.Status.String() == "Running" {
+				 timeEndStr = timeStart.Add(taskInfo.TimeLimit.AsDuration()).String()
+			 }
+			 var timeLimitStr string
+			 if taskInfo.TimeLimit.Seconds >= util.InvalidDuration().Seconds {
+				 timeLimitStr = "unlimited"
+				 timeEndStr = "unknown"
+			 } else {
+				 timeLimitStr = util.SecondTimeFormat(taskInfo.TimeLimit.Seconds)
+			 }
+ 
+			 fmt.Printf("JobId=%v JobName=%v\n\tUserId=%d GroupId=%d Account=%v\n\tJobState=%v RunTime=%v "+
+				 "TimeLimit=%s SubmitTime=%v\n\tStartTime=%v EndTime=%v Partition=%v NodeList=%v "+
+				 "NumNodes=%d\n\tCmdLine=%v Workdir=%v\n",
+				 taskInfo.TaskId, taskInfo.Name, taskInfo.Uid, taskInfo.Gid,
+				 taskInfo.Account, taskInfo.Status.String(), runTime, timeLimitStr,
+				 timeSubmitStr, timeStartStr, timeEndStr, taskInfo.Partition,
+				 taskInfo.CranedList, taskInfo.NodeNum, taskInfo.CmdLine, taskInfo.Cwd)
+		 }
+	 }
+ }
+ 
+ func ShowConfig(ConfigPath string) {
+	 yamlFile, err := ioutil.ReadFile(ConfigPath)
+	 if err != nil {
+		 log.Fatalf("Failed to read configuration YAML file: %v", err)
+	 }
+	 var yamlData yaml.MapSlice
+	 err = yaml.Unmarshal(yamlFile, &yamlData)
+	 if err != nil {
+		 log.Fatalf("Failed to unmarshal configuration YAML: %v", err)
+	 }
+	 for _, line := range yamlData {
+		 LineKey := line.Key.(string)
+		 LineValue := line.Value
+		 if reflect.TypeOf(LineValue).Kind() != reflect.Slice {
+			 fmt.Printf("%s = %v\n", LineKey, LineValue)
+		 } else {
+			 fmt.Printf("%s:\n", LineKey)
+			 LineLength := reflect.ValueOf(LineValue).Len()
+			 for ValueIndex := 0; ValueIndex < LineLength; ValueIndex++ {
+				 Item := reflect.ValueOf(LineValue).Index(ValueIndex).Interface()
+				 ItemLength := reflect.ValueOf(Item).Len()
+				 for ItemIndex := 0; ItemIndex < ItemLength; ItemIndex++ {
+					 ItemStruct := reflect.ValueOf(Item).Index(ItemIndex).Interface()
+					 MapStruct, ok := ItemStruct.(yaml.MapItem)
+					 if ok {
+						 if ItemIndex == 0 {
+							 fmt.Printf("\t- ")
+						 } else {
+							 fmt.Printf("\t  ")
+						 }
+						 fmt.Printf("%s = %v\n", MapStruct.Key, MapStruct.Value)
+					 }
+				 }
+			 }
+		 }
+	 }
+ }
+ 
+ func ChangeTaskTimeLimit(taskId uint32, timeLimit string) {
+	 re := regexp.MustCompile(`((.*)-)?(.*):(.*):(.*)`)
+	 result := re.FindAllStringSubmatch(timeLimit, -1)
+ 
+	 if result == nil || len(result) != 1 {
+		 log.Fatalf("Time format error")
+	 }
+	 var dd uint64
+	 if result[0][2] != "" {
+		 dd, _ = strconv.ParseUint(result[0][2], 10, 32)
+	 }
+	 hh, err := strconv.ParseUint(result[0][3], 10, 32)
+	 if err != nil {
+		 log.Fatalf("The hour time format error")
+	 }
+	 mm, err := strconv.ParseUint(result[0][4], 10, 32)
+	 if err != nil {
+		 log.Fatalf("The minute time format error")
+	 }
+	 ss, err := strconv.ParseUint(result[0][5], 10, 32)
+	 if err != nil {
+		 log.Fatalf("The second time format error")
+	 }
+ 
+	 seconds := int64(60*60*24*dd + 60*60*hh + 60*mm + ss)
+ 
+	 var req *protos.ModifyTaskRequest
+ 
+	 req = &protos.ModifyTaskRequest{
+		 Uid:       uint32(os.Getuid()),
+		 TaskId:    taskId,
+		 Attribute: protos.ModifyTaskRequest_TimeLimit,
+		 Value: &protos.ModifyTaskRequest_TimeLimitSeconds{
+			 TimeLimitSeconds: seconds,
+		 },
+	 }
+	 reply, err := stub.ModifyTask(context.Background(), req)
+	 if err != nil {
+		 util.GrpcErrorPrintf(err, "Failed to change task time limit")
+	 }
+ 
+	 if reply.Ok {
+		 fmt.Println("Change time limit success")
+	 } else {
+		 fmt.Printf("Change time limit failed: %s\n", reply.GetReason())
+	 }
+ }
+ 
+ func ChangeTaskPriority(taskId uint32, priority uint32) {
+	 var req *protos.ModifyTaskRequest
+	 req = &protos.ModifyTaskRequest{
+		 Uid:       uint32(os.Getuid()),
+		 TaskId:    taskId,
+		 Attribute: protos.ModifyTaskRequest_Priority,
+		 Value: &protos.ModifyTaskRequest_PriorityValue{
+			 PriorityValue: priority,
+		 },
+	 }
+	 reply, err := stub.ModifyTask(context.Background(), req)
+	 if err != nil {
+		 util.GrpcErrorPrintf(err, "Failed to change task priority")
+	 }
+ 
+	 if reply.Ok {
+		 fmt.Println("Change priority success")
+	 } else {
+		 fmt.Printf("Change priority failed: %s\n", reply.GetReason())
+	 }
+ }
+ 
