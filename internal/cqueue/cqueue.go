9231936f110ce097983def45bdab2d21a793f024
Nativu5 <44155313+Nativu5@users.noreply.github.com>
2024-06-25 00:07:04 +0800

    feat: Add REASON column

--- internal/cqueue/cqueue.go
+++ internal/cqueue/cqueue.go
@@ -110,7 +110,7 @@ func Query() util.CraneCmdError {
 	table := tablewriter.NewWriter(os.Stdout)
 	util.SetBorderlessTable(table)
 	header := []string{"JobId", "Partition", "Name", "User",
-		"Account", "Status", "Type", "Time", "TimeLimit", "Nodes", "NodeList"}
+		"Account", "Status", "Type", "Time", "TimeLimit", "Nodes", "NodeList/Reason"}
 	tableData := make([][]string, len(reply.TaskInfoList))
 	for i := 0; i < len(reply.TaskInfoList); i++ {
 		var timeLimitStr string
@@ -251,7 +251,7 @@ func FormatData(reply *protos.QueryTasksInfoReply) (header []string, tableData [
 				formatTableData[j] = append(formatTableData[j], reply.TaskInfoList[j].Type.String())
 			}
 		case "I":
-			tableOutputHeader[i] = "NodeList"
+			tableOutputHeader[i] = "NodeList/Reason"
 			for j := 0; j < len(reply.TaskInfoList); j++ {
 				formatTableData[j] = append(formatTableData[j], reply.TaskInfoList[j].CranedList)
 			}
