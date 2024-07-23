6012a2eb2880be651b31bef7721da5fc50aaa7f9
Harvey Zhou <125181511+JingxuanZhouNo1@users.noreply.github.com>
2024-07-23 20:15:50 +0800

    feat: Support for print Reason/Nodelists in "cqueue --format" (#125)
    * Add(CmdParser, cqueue):add %r
    
    * Add(CmdParser, cqueue):advised on 7,July,2024
    
    * Add(CmdParer, cqueue):advised on 23,Jul,2024
    
    * Add(CmdParer, cqueue):advised_2
    
    * fix: Typos
    
    * fix: File permissions
    
    ---------
    
    Co-authored-by: Yongkun Li <44155313+Nativu5@users.noreply.github.com>

--- internal/cqueue/CmdArgParser.go
+++ internal/cqueue/CmdArgParser.go
@@ -112,13 +112,15 @@ Supported format identifiers:
 	%L: NodeList   - Display the list of nodes the job is running on.
 	%n: Name       - Display the name of the job.
 	%N: Nodes      - Display the number of nodes assigned to the job.
-	%p: Priority   - Display the job's priority.
+	%p: Priority   - Display the priority of the job.
 	%P: Partition  - Display the partition the job is running in.
 	%q: QoS        - Display the Quality of Service level for the job.
+	%r: Reason     - Display the reason of pending.
 	%s: SubmitTime - Display the submission time of the job.
 	%t: State      - Display the current state of the job.
 	%T: Type       - Display the job type.
 	%u: User       - Display the user who submitted the job.
+	 
 
 Each format specifier can be modified with a width specifier (e.g., "%.5j").
 If the width is specified, the field will be formatted to at least that width. 
