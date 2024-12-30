506af07673c0b81f62a6da9c6ed2012590e6c8c4
huerni <47264950+huerni@users.noreply.github.com>
2024-12-30 13:46:15 +0800

    feat: operator add PKI Authentication

--- internal/cacct/CmdArgParser.go
+++ internal/cacct/CmdArgParser.go
@@ -54,11 +54,6 @@ var (
 			util.DetectNetworkProxy()
 			config := util.ParseConfig(FlagConfigFilePath)
 			stub = util.GetStubToCtldByConfig(config)
-
-			if err := util.SignAndSaveUserCertificate(config); err != util.ErrorSuccess {
-				os.Exit(err)
-			}
-
 		},
 		Run: func(cmd *cobra.Command, args []string) {
 			if cmd.Flags().Changed("max-lines") {
