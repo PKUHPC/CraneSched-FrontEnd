3a650af67964bf2de1d1dcd12ca168d2855d3610
1daidai1 <166272788+1daidai1@users.noreply.github.com>
2025-02-27 22:32:57 +0800

    feat: Add specific reasons for failed task submission (#218)
    * Add specific reasons for failed task submission
    
    * change proto
    
    * change proto errcode
    
    * fix qos resource enum
    
    * fix comments
    
    * refactor account errcode name
    
    * style: Change CraneErrCode::__ERR_CODE_COUNT
    
    Signed-off-by: lijunlin <xiafeng.li@foxmail.com>
    
    ---------
    
    Signed-off-by: lijunlin <xiafeng.li@foxmail.com>
    Co-authored-by: huerni <47264950+huerni@users.noreply.github.com>
    Co-authored-by: lijunlin <xiafeng.li@foxmail.com>

--- internal/util/err.go
+++ internal/util/err.go
@@ -37,95 +37,78 @@ const (
 	ErrorInvalidFormat CraneCmdError = 5
 )
 
+var errMsgMap = map[protos.ErrCode]string{
+
+	// User-related errors
+	protos.ErrCode_ERR_INVALID_UID:                   "The user UID being operated on does not exist in the system",
+	protos.ErrCode_ERR_INVALID_OP_USER:               "You are not a user of Crane",
+	protos.ErrCode_ERR_INVALID_USER:                  "The entered user is not a user of Crane",
+	protos.ErrCode_ERR_PERMISSION_USER:               "Your permission is insufficient",
+	protos.ErrCode_ERR_BLOCKED_USER:                  "The user has been blocked",
+	protos.ErrCode_ERR_USER_ALREADY_EXISTS:           "The user already exists in this account",
+	protos.ErrCode_ERR_USER_ACCESS_TO_ACCOUNT_DENIED: "The user is not allowed to access account",
+	protos.ErrCode_ERR_INVALID_ADMIN_LEVEL:           "Unknown admin level",
+	protos.ErrCode_ERR_USER_ACCOUNT_MISMATCH:         "The user does not belong to this account",
+	protos.ErrCode_ERR_NO_ACCOUNT_SPECIFIED:          "No account is specified for the user",
+
+	// Account-related errors
+	protos.ErrCode_ERR_INVALID_ACCOUNT:        "The entered account does not exist",
+	protos.ErrCode_ERR_INVALID_PARENT_ACCOUNT: "The parent account of the entered account does not exist",
+	protos.ErrCode_ERR_ACCOUNT_ALREADY_EXISTS: "The account already exists in the crane",
+	protos.ErrCode_ERR_ACCOUNT_HAS_CHILDREN:   "The account has child account or users, unable to delete.",
+	protos.ErrCode_ERR_BLOCKED_ACCOUNT:        "The account has been blocked",
+
+	// Partition-related errors
+	protos.ErrCode_ERR_INVALID_PARTITION:                "The entered partition does not exist",
+	protos.ErrCode_ERR_PARTITION_MISSING:                "The entered account or user does not include this partition",
+	protos.ErrCode_ERR_PARENT_ACCOUNT_PARTITION_MISSING: "Parent account does not include the partition",
+	protos.ErrCode_ERR_PARTITION_ALREADY_EXISTS:         "The partition already exists in the account or user",
+	protos.ErrCode_ERR_USER_EMPTY_PARTITION:             "The user does not contain any partitions, operation cannot be performed.",
+	protos.ErrCode_ERR_CHILD_HAS_PARTITION:              "The partition is currently being used by the child accounts or users of the account, operation cannot be performed. You can use a forced operation to ignore this constraint",
+	protos.ErrCode_ERR_HAS_NO_QOS_IN_PARTITION:          "The user has no QoS available for this partition to be used",
+	protos.ErrCode_ERR_HAS_ALLOWED_QOS_IN_PARTITION:     "The qos you set is not in partition's allowed qos list",
+
+	// QoS-related errors
+	protos.ErrCode_ERR_INVALID_QOS:                     "The entered qos does not exist",
+	protos.ErrCode_ERR_DB_QOS_ALREADY_EXISTS:           "Qos already exists in the crane",
+	protos.ErrCode_ERR_QOS_REFERENCES_EXIST:            "QoS is still being used by accounts or users, unable to delete",
+	protos.ErrCode_ERR_CONVERT_TO_INTEGER:              "Failed to convert value to integer",
+	protos.ErrCode_ERR_TIME_LIMIT:                      "Invalid time limit value",
+	protos.ErrCode_ERR_QOS_MISSING:                     "The entered account or user does not include this qos",
+	protos.ErrCode_ERR_QOS_ALREADY_EXISTS:              "The Qos already exists in the account or user",
+	protos.ErrCode_ERR_PARENT_ACCOUNT_QOS_MISSING:      "Parent account does not include the qos",
+	protos.ErrCode_ERR_SET_ALLOWED_QOS:                 "The entered QoS list does not include the default QoS for this user. Ignoring this constraint with forced operation, the default QoS is randomly replaced with one of the items in the new QoS list",
+	protos.ErrCode_ERR_DEFAULT_QOS_NOT_INHERITED:       "The entered default_qos is not allowed",
+	protos.ErrCode_ERR_DUPLICATE_DEFAULT_QOS:           "The QoS is already the default QoS for the account or specified partition of the user",
+	protos.ErrCode_ERR_SET_ACCOUNT_QOS:                 "The entered QoS list does not include the default QoS for this account or some descendant node. You can use a forced operation to ignore this constraint",
+	protos.ErrCode_ERR_CHILD_HAS_DEFAULT_QOS:           "some child accounts or users is using the QoS as the default QoS. By ignoring this constraint with forced deletion, the deleted default QoS is randomly replaced with one of the remaining items in the QoS list",
+	protos.ErrCode_ERR_SET_DEFAULT_QOS:                 "The Qos not allowed or is already the default qos",
+	protos.ErrCode_ERR_DEFAULT_QOS_MODIFICATION_DENIED: "The QoS is the default QoS for the current user/Account and cannot be modified. Ignoring this constraint with forced operation, the default QoS is randomly replaced with one of the items in the new QoS list",
+
+	// System-related errors
+	protos.ErrCode_ERR_UPDATE_DATABASE:        "Fail to update data in database",
+	protos.ErrCode_ERR_NO_RESOURCE:            "Resource not enough for task",
+	protos.ErrCode_ERR_INVALID_NODE_NUM:       "Nodes partition not enough for task",
+	protos.ErrCode_ERR_INVAILD_NODE_LIST:      "Invalid node list",
+	protos.ErrCode_ERR_INVAILD_EX_NODE_LIST:   "Invalid exclude node list",
+	protos.ErrCode_ERR_TIME_TIMIT_BEYOND:      "Time-limit reached the user's limit",
+	protos.ErrCode_ERR_CPUS_PER_TASK_BEYOND:   "cpus-per-task reached the user's limit",
+	protos.ErrCode_ERR_NO_ENOUGH_NODE:         "Nodes num not enough for task",
+	protos.ErrCode_ERR_BEYOND_TASK_ID:         "System error occurred or the number of pending tasks exceeded maximum value",
+	protos.ErrCode_ERR_CGROUP:                 "Error when manipulating cgroup",
+	protos.ErrCode_ERR_SYSTEM_ERR:             "Linux Error",
+	protos.ErrCode_ERR_RPC_FAILURE:            "RPC call failed",
+	protos.ErrCode_ERR_GENERIC_FAILURE:        "Generic failure",
+	protos.ErrCode_ERR_NON_EXISTENT:           "The object doesn't exist",
+	protos.ErrCode_ERR_INVALID_PARAM:          "Invalid Parameter",
+	protos.ErrCode_ERR_PROTOBUF:               "Error when using protobuf",
+	protos.ErrCode_ERR_MAX_JOB_COUNT_PER_USER: "job max count is empty or exceeds the limit",
+	protos.ErrCode_ERR_USER_NO_PRIVILEGE:      "User has insufficient privilege",
+}
+
 func ErrMsg(err_code protos.ErrCode) string {
-	switch err_code {
-	case protos.ErrCode_ERR_INVALID_UID:
-		return "The user UID being operated on does not exist in the system"
-	case protos.ErrCode_ERR_INVALID_OP_USER:
-		return "you are not a user of Crane"
-	case protos.ErrCode_ERR_INVALID_USER:
-		return "The entered user is not a user of Crane"
-	case protos.ErrCode_ERR_PERMISSION_USER:
-		return "Your permission is insufficient"
-	case protos.ErrCode_ERR_USER_DUPLICATE_ACCOUNT:
-		return "The user already exists in this account"
-	case protos.ErrCode_ERR_USER_ALLOWED_ACCOUNT:
-		return "The user is not allowed to access account"
-	case protos.ErrCode_ERR_INVALID_ADMIN_LEVEL:
-		return "Unknown admin level"
-	case protos.ErrCode_ERR_USER_ACCOUNT_MISMATCH:
-		return "The user does not belong to this account"
-	case protos.ErrCode_ERR_NO_ACCOUNT_SPECIFIED:
-		return "No account is specified for the user"
-	default:
-		break
+	if msg, exists := errMsgMap[err_code]; exists {
+		return msg
 	}
-
-	switch err_code {
-	case protos.ErrCode_ERR_INVALID_ACCOUNT:
-		return "The entered account does not exist"
-	case protos.ErrCode_ERR_INVALID_PARENTACCOUNT:
-		return "The parent account of the entered account does not exist"
-	case protos.ErrCode_ERR_DUPLICATE_ACCOUNT:
-		return "The account already exists in the crane"
-	case protos.ErrCode_ERR_DELETE_ACCOUNT:
-		return "The account has child account or users, unable to delete."
-	}
-
-	switch err_code {
-	case protos.ErrCode_ERR_INVALID_PARTITION:
-		return "The entered partition does not exist"
-	case protos.ErrCode_ERR_ALLOWED_PARTITION:
-		return "The entered account or user does not include this partition"
-	case protos.ErrCode_ERR_PARENT_ALLOWED_PARTITION:
-		return "Parent account does not include the partition"
-	case protos.ErrCode_ERR_DUPLICATE_PARTITION:
-		return "The partition already exists in the account or user"
-	case protos.ErrCode_ERR_USER_EMPTY_PARTITION:
-		return "The user does not contain any partitions, operation cannot be performed."
-	case protos.ErrCode_ERR_CHILD_HAS_PARTITION:
-		return "The partition is currently being used by the child accounts or users of the account, operation cannot be performed. You can use a forced operation to ignore this constraint"
-	}
-
-	switch err_code {
-	case protos.ErrCode_ERR_INVALID_QOS:
-		return "The entered qos does not exist"
-	case protos.ErrCode_ERR_DB_DUPLICATE_QOS:
-		return "Qos already exists in the crane"
-	case protos.ErrCode_ERR_DELETE_QOS:
-		return "QoS is still being used by accounts or users, unable to delete"
-	case protos.ErrCode_ERR_CONVERT_TO_INTERGER:
-		return "Failed to convert value to integer"
-	case protos.ErrCode_ERR_TIME_LIMIT:
-		return "Invalid time limit value"
-	case protos.ErrCode_ERR_ALLOWED_QOS:
-		return "The entered account or user does not include this qos"
-	case protos.ErrCode_ERR_DUPLICATE_QOS:
-		return "The Qos already exists in the account or user"
-	case protos.ErrCode_ERR_PARENT_ALLOWED_QOS:
-		return "Parent account does not include the qos"
-	case protos.ErrCode_ERR_SET_ALLOWED_QOS:
-		return "The entered QoS list does not include the default QoS for this user. Ignoring this constraint with forced operation, the default QoS is randomly replaced with one of the items in the new QoS list"
-	case protos.ErrCode_ERR_ALLOWED_DEFAULT_QOS:
-		return "The entered default_qos is not allowed"
-	case protos.ErrCode_ERR_DUPLICATE_DEFAULT_QOS:
-		return "The QoS is already the default QoS for the account or specified partition of the user"
-	case protos.ErrCode_ERR_SET_ACCOUNT_QOS:
-		return "The entered QoS list does not include the default QoS for this account or some descendant node. You can use a forced operation to ignore this constraint"
-	case protos.ErrCode_ERR_CHILD_HAS_DEFAULT_QOS:
-		return "some child accounts or users is using the QoS as the default QoS. By ignoring this constraint with forced deletion, the deleted default QoS is randomly replaced with one of the remaining items in the QoS list"
-	case protos.ErrCode_ERR_SET_DEFAULT_QOS:
-		return "The Qos not allowed or is already the default qos"
-	case protos.ErrCode_ERR_IS_DEFAULT_QOS:
-		return "The QoS is the default QoS for the current user/Account and cannot be modified. Ignoring this constraint with forced operation, the default QoS is randomly replaced with one of the items in the new QoS list"
-	}
-
-	switch err_code {
-	case protos.ErrCode_ERR_UPDATE_DATABASE:
-		return "Fail to update data in database"
-	default:
-		break
-	}
-
 	return fmt.Sprintf("Unknown Error Occurred: %s", err_code)
 }
