a96fd33be90c3b0b504894c064595cdc8197dafb
db <1301189887@qq.com>
2025-05-19 16:58:54 +0800

    fix comments

--- internal/crun/crun.go
+++ internal/crun/crun.go
@@ -1184,7 +1184,7 @@ func MainCrun(cmd *cobra.Command, args []string) error {
 			log.Errorf("Invalid argument: %v", err)
 			return util.ErrorCmdArg
 		}
-		task.MemPerCpu = memInBytePerCpu
+		*task.MemPerCpu = memInBytePerCpu
 	}
 	if FlagGres != "" {
 		gresMap := util.ParseGres(FlagGres)
