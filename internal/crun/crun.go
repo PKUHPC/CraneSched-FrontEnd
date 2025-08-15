2d85102761f275ddb6647d9e89ce4430a71bc494
lijunlin <xiafeng.li@foxmail.com>
2025-08-15 16:30:00 +0800

    fix: username
    Signed-off-by: lijunlin <xiafeng.li@foxmail.com>

--- internal/crun/crun.go
+++ internal/crun/crun.go
@@ -807,7 +807,7 @@ func (m *StateMachineOfCrun) ParseFilePattern(pattern string) (string, error) {
 		//task identifier (rank) relative to current job.
 		//"%t": "0",
 		//User name
-		"%u": currentUser.Name,
+		"%u": currentUser.Username,
 		// Job name
 		"%x": m.task.Name,
 	}
