fb05f7ae5ac8c7a2e5557a04e6575a36c7760686
huerni <47264950+huerni@users.noreply.github.com>
2025-08-28 10:35:41 +0800

    todo

--- internal/crun/crun.go
+++ internal/crun/crun.go
@@ -528,7 +528,7 @@ func (m *StateMachineOfCrun) StateForwarding() {
 						"Killing job...", err)
 					gVars.connectionBroken = true
 					m.err = util.ErrorNetwork
-					m.state = TaskKilling
+					m.state = End
 				}
 			} else {
 				switch cforedReply.Type {
