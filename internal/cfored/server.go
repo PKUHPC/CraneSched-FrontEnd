7de825604840f3a9e8a8a4ca66f214926ac8662f
huerni <47264950+huerni@users.noreply.github.com>
2025-09-02 16:22:23 +0800

    feat

--- internal/cfored/server.go
+++ internal/cfored/server.go
@@ -221,6 +221,7 @@ const (
 	SupervisorUnReg StateOfCranedServer = 2
 )
 
+// TODO:重新接上stream
 func (cforedServer *GrpcCforedServer) TaskIOStream(toSupervisorStream protos.CraneForeD_TaskIOStreamServer) error {
 	var cranedId string
 	var jobId uint32
