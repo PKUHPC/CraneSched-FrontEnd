12569e4adf9b50cd925a33f98c649694108a8d96
edragain2nd <edragain@163.com>
2025-10-09 23:44:37 +0800

    adjust detail

--- internal/cbatch/cmd.go
+++ internal/cbatch/cmd.go
@@ -174,5 +174,5 @@ func init() {
 	RootCmd.Flags().BoolVar(&FlagExclusive, "exclusive", false, "Exclusive node resources")
 	RootCmd.Flags().BoolVarP(&FlagHold, "hold", "H", false, "Hold the job until it is released")
 	RootCmd.Flags().StringVarP(&FlagBeginTime, "begin", "b", "", "Defer job until specified time.")
-	RootCmd.Flags().StringVar(&FlagDeadlineTime, "deadline", "", "Remove the job if not started by time.")
+	RootCmd.Flags().StringVar(&FlagDeadlineTime, "deadline", "", "Cancel the job if it reaches its deadline.")
 }
