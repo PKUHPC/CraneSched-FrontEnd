0b5707f6e93fd4a1af099cac8a308f39e5b8f8fc
bigSheep123 <1554869970@qq.com>
2025-10-09 11:52:55 +0800

    newCraneErr

--- internal/cbatch/cbatch.go
+++ internal/cbatch/cbatch.go
@@ -357,10 +357,7 @@ func SendRequest(task *protos.TaskToCtld) error {
 		fmt.Printf("Job id allocated: %d.\n", reply.GetTaskId())
 		return nil
 	} else {
-		return &util.CraneError{
-			Code:    util.ErrorBackend,
-			Message: fmt.Sprintf("Job allocation failed: %s.", util.ErrMsg(reply.GetCode())),
-		}
+		return util.NewCraneErr(util.ErrorBackend,fmt.Sprintf("Job allocation failed: %s.", util.ErrMsg(reply.GetCode())))
 	}
 }
 
@@ -403,10 +400,7 @@ func SendMultipleRequests(task *protos.TaskToCtld, count uint32) error {
 func ParseCbatchScript(path string, args *[]CbatchArg, sh *[]string) error {
 	file, err := os.Open(path)
 	if err != nil {
-		return &util.CraneError{
-			Code:    util.ErrorCmdArg,
-			Message: err.Error(),
-		}
+		return util.NewCraneErr(util.ErrorCmdArg,err.Error())
 	}
 	defer func(file *os.File) {
 		err := file.Close()
@@ -448,18 +442,12 @@ func ParseCbatchScript(path string, args *[]CbatchArg, sh *[]string) error {
 		}
 		err := processor.Process(scanner.Text(), sh, args)
 		if err != nil {
-			return &util.CraneError{
-				Code:    util.ErrorCmdArg,
-				Message: fmt.Sprintf("Parsing error at line %d: %s", num, err),
-			}
+			return util.NewCraneErr(util.ErrorCmdArg,fmt.Sprintf("Parsing error at line %d: %s", num, err))
 		}
 	}
 
 	if err := scanner.Err(); err != nil {
-		return &util.CraneError{
-			Code:    util.ErrorCmdArg,
-			Message: err.Error(),
-		}
+		return util.NewCraneErr(util.ErrorCmdArg,err.Error())
 	}
 
 	return nil
