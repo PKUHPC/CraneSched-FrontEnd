a160f658d4d63bc4cad25245f97a1e03232ba3a8
db <1301189887@qq.com>
2025-11-05 15:07:24 +0800

    add cacct/cqueue nodesname

--- internal/cacct/cacct.go
+++ internal/cacct/cacct.go
@@ -167,6 +167,17 @@ func QueryJob() error {
 		request.FilterTaskTypes = filterTaskTypeList
 	}
 
+	if FlagFilterNodesName != "" {
+		filterNodeNameList, ok := util.ParseHostList(FlagFilterNodesName)
+		if !ok {
+			return &util.CraneError{
+				Code:    util.ErrorCmdArg,
+				Message: fmt.Sprintf("Invalid node pattern: %s.", filterNodeNameList),
+			}
+		}
+		request.FilterNodesName = filterNodeNameList
+	}
+
 	if FlagNumLimit != 0 {
 		request.NumLimit = FlagNumLimit
 	}
