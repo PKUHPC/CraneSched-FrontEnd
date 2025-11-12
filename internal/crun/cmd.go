53848aa9d2c468c4e40487e69d5682c00780543e
Junlin Li <70465472+l-xiafeng@users.noreply.github.com>
2025-11-12 14:48:46 +0800

    fix: x11 multi conn
    Signed-off-by: lijunlin <xiafeng.li@foxmail.com>
    
    fix: X11 multi conn
    
    Signed-off-by: lijunlin <xiafeng.li@foxmail.com>
    
    feat: X11 multi conn
    
    Signed-off-by: lijunlin <xiafeng.li@foxmail.com>
    
    task exit status
    
    Signed-off-by: lijunlin <xiafeng.li@foxmail.com>
    
    fix: Crun app args
    
    Signed-off-by: lijunlin <xiafeng.li@foxmail.com>
    
    sync protos
    
    Signed-off-by: lijunlin <xiafeng.li@foxmail.com>
    
    feat: Introcduce multi level cgrop for step/task
    Signed-off-by: lijunlin <xiafeng.li@foxmail.com>
    
    fix:step erro msg

--- internal/crun/cmd.go
+++ internal/crun/cmd.go
@@ -109,6 +109,7 @@ func ParseCmdArgs() {
 }
 
 func init() {
+	RootCmd.Flags().SetInterspersed(false)
 	RootCmd.SetVersionTemplate(util.VersionTemplate())
 	RootCmd.PersistentFlags().StringVarP(&FlagConfigFilePath, "config", "C", util.DefaultConfigPath, "Path to configuration file")
 	RootCmd.PersistentFlags().StringVarP(&FlagDebugLevel, "debug-level", "",
