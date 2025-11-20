76f67904962491f0249703baf7d472c003f05b89
lijunlin <xiafeng.li@foxmail.com>
2025-11-20 16:38:29 +0800

    fix: Crun app args
    Signed-off-by: lijunlin <xiafeng.li@foxmail.com>

--- internal/crun/cmd.go
+++ internal/crun/cmd.go
@@ -78,6 +78,7 @@ func ParseCmdArgs() {
 }
 
 func init() {
+	RootCmd.Flags().SetInterspersed(false)
 	RootCmd.SetVersionTemplate(util.VersionTemplate())
 	RootCmd.PersistentFlags().StringVarP(&FlagConfigFilePath, "config", "C", util.DefaultConfigPath, "Path to configuration file")
 	RootCmd.PersistentFlags().StringVarP(&FlagDebugLevel, "debug-level", "",
