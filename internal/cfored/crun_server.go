da739bac033a25b2e5102cf285264031c4fd11f1
huerni <47264950+huerni@users.noreply.github.com>
2025-12-30 10:37:51 +0800

    merge master

--- internal/cfored/crun_server.go
+++ internal/cfored/crun_server.go
@@ -323,7 +323,7 @@ CforedCrunStateMachineLoop:
 					gVars.pidStepMapMtx.Lock()
 					gVars.pidStepMap[crunPid] = step
 					gVars.pidStepMapMtx.Unlock()
-					gSupervisorChanKeeper.setRemoteIoToCrunChannel(jobId, stepId, StepIoRequestChannel)
+					gSupervisorChanKeeper.setRemoteIoToCrunChannel(crunPid, jobId, stepId, StepIoRequestChannel)
 				}
 
 				gVars.ctldReplyChannelMapMtx.Unlock()
