8f9fef0550c10d64d779c58c8d264fadb725a1b3
zhansan114514 <1727448271@qq.com>
2026-04-04 21:45:08 +0800

    fix

--- internal/ccontrol/ccontrol.go
+++ internal/ccontrol/ccontrol.go
@@ -207,6 +207,10 @@ func SuspendJobs(jobs string) error {
 
 	if FlagJson {
 		fmt.Println(util.FmtJson.FormatReply(reply))
+		// Check if there are any failures and return appropriate error code
+		if len(reply.NotModifiedJobs) > 0 {
+			return &util.CraneError{Code: util.ErrorBackend}
+		}
 		return nil
 	}
 
@@ -234,6 +238,10 @@ func ResumeJobs(jobs string) error {
 
 	if FlagJson {
 		fmt.Println(util.FmtJson.FormatReply(reply))
+		// Check if there are any failures and return appropriate error code
+		if len(reply.NotModifiedJobs) > 0 {
+			return &util.CraneError{Code: util.ErrorBackend}
+		}
 		return nil
 	}
 
