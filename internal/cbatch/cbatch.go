6c445b45e348d336e8b13034d1c916f4aa432365
junlinli <xiafeng.li@foxmail.com>
2026-04-16 13:21:59 +0800

    feat: Requeue

--- internal/cbatch/cbatch.go
+++ internal/cbatch/cbatch.go
@@ -274,6 +274,9 @@ func BuildCbatchJob(cmd *cobra.Command, args []string, config *util.Config) (*pr
 	if FlagHold {
 		job.Hold = true
 	}
+	if FlagRequeue {
+		job.RequeueIfFailed = true
+	}
 	if FlagReservation != "" {
 		job.Reservation = FlagReservation
 	}
@@ -534,6 +537,10 @@ func applyScriptArgs(cmd *cobra.Command, cbatchArgs []CbatchArg, job *protos.Job
 			for _, signal := range signals {
 				job.Signals = append(job.Signals, signal)
 			}
+		case "--requeue":
+			job.RequeueIfFailed = true
+		case "--no-requeue":
+			job.RequeueIfFailed = false
 		default:
 			return fmt.Errorf("invalid argument: unrecognized '%s' in script", arg.name)
 		}
@@ -667,7 +674,6 @@ func ParseCbatchScript(path string, args *[]CbatchArg, sh *[]string) error {
 func FilterDummyArgs(args []CbatchArg) []CbatchArg {
 	filteredArgs := make([]CbatchArg, 0, len(args))
 	unsupportedFlags := map[string]string{
-		"no-requeue":        "The feature --no-requeue is not yet supported by Crane, the use is ignored.",
 		"parsable":          "The feature --parsable is not yet supported by Crane, the use is ignored.",
 		"gpus-per-node":     "The feature --gpus-per-node is not yet supported by Crane, the use is ignored.",
 		"ntasks-per-socket": "The feature --ntasks-per-socket is not yet supported by Crane, the use is ignored.",
@@ -682,7 +688,6 @@ func FilterDummyArgs(args []CbatchArg) []CbatchArg {
 		"i":                 "The feature --input/-i is not yet supported by Crane, the use is ignored.",
 		"sockets-per-node":  "The feature --sockets-per-node is not yet supported by Crane, the use is ignored.",
 		"cores-per-socket":  "The feature --cores-per-socket is not yet supported by Crane, the use is ignored.",
-		"requeue":           "The feature --requeue is not yet supported by Crane, the use is ignored.",
 		"wait":              "The feature --wait/-W is not yet supported by Crane, the use is ignored.",
 		"W":                 "The feature --wait/-W is not yet supported by Crane, the use is ignored.",
 	}
