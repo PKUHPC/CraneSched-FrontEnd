e99280f28938938cc799c771a200269c6ef4db9e
huerni <47264950+huerni@users.noreply.github.com>
2026-04-27 16:40:22 +0800

    fix(cattach): align RootCmd variable declaration for improved readability

--- internal/cattach/cmd.go
+++ internal/cattach/cmd.go
@@ -19,7 +19,7 @@ var (
 	// (i.e., crun was started with --input=<task_id>).  In read-only mode cattach displays
 	// output but does not forward any stdin to the running tasks.
 	FlagReadOnly bool
-	RootCmd            = &cobra.Command{
+	RootCmd      = &cobra.Command{
 		Use:     "cattach [flags] jobid.stepid",
 		Short:   "Attach to a crane job step",
 		Version: util.Version(),
