3f84382ccdb57659ad84a56cc8f01e4ffeb9f751
Junlin Li <70465472+L-Xiafeng@users.noreply.github.com>
2026-08-20 11:51:12 +0800

    Revert "fix: register cfored with short hostname (#454)" (#459)
    This reverts commit cdbfb60f4a09028188d915e0970badb2c08fd569.

--- internal/cfored/cfored.go
+++ internal/cfored/cfored.go
@@ -161,8 +161,7 @@ func StartCfored(cmd *cobra.Command) {
 	if err != nil {
 		log.Fatalf("Failed to get hostname: %s", err.Error())
 	}
-	gVars.hostName = util.GetShortHostname(hostName)
-	log.Infof("Using %s as cfored name (system hostname: %s).", gVars.hostName, hostName)
+	gVars.hostName = hostName
 
 	var wgAllRoutines sync.WaitGroup
 
