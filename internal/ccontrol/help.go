2d7df650b0d18a44dd558369dcf1e27732c91c56
Victor Zhou <zhouhao3290@gmail.com>
2026-08-28 16:11:41 +0800

    update

--- internal/ccontrol/help.go
+++ internal/ccontrol/help.go
@@ -136,7 +136,7 @@ COMMANDS:
 
 GLOBAL OPTIONS:
   --help, -h     Display this help message
-  -V, --version  Show version information
+  --version, -v  Show version information
   --json, -J     Format output as JSON
   --config, -C   Specify an alternative configuration file
   
